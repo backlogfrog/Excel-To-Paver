3d69a1b454759ee2112b46c0feef52a67e14bb2c
about blank <aboutb@gmail.com>
2021-02-10 20:13:01 +0000

    Won't print distresscodes

--- scratchXml.py
+++ scratchXml.py
@@ -2,13 +2,11 @@
 #custom XML module YATTAG wouldn't take data correctly so I said Fuck it and XML will be written from scratch
 #Distress comment for every distress listed
 DistressComment = "Imported"
-#severity of alligator cracking based on 1,2,3 from excel sheet, mapping.py column locations - 1=Low 2=Med 3=High, use L M H
-alligator_Severity = "NONE"
-potHole_Severity = "NONE"
-
-
-#sampleNum
-#distressQuantity
+#row[INSPECTED_SIZE]-- set actual square footage - excel sheet has different size
+#inspectedSize=row[P_LENGTH]*row[P_WIDTH]
+inspectedSize = 12
+#PCI condition, doesn't import
+condition=1
 
 
 #
@@ -58,16 +56,14 @@ dateSet="01/01/2010"
 
 #print(xml_header, "\n", xml_schema, "\n", file=f) and opening xml tags
 
-#row[INSPECTED_SIZE]-- set actual square footage - excel sheet has different size
-#inspectedSize=row[P_LENGTH]*row[P_WIDTH]
 
 #manually write XML data to filename.xml
 #Changed comment to say Imp: for imported to identify
 
 
 
-print(iS, "<geospatialInspectionData inspectionDate=\"",dateSet,"\"", " units=\"English\" level=\"FRAME\" >", sep="", file=f)
-print(iS*2, "<inspectedElement inspectedElementID=\"", row[INSPECTED_PID2], "\"", " size=\"", inspectedSize, "\" ", "PID=\"", fullpid, "\" inspectedType=\"R\" comment=\"",DistressComment,  "\" noDistresses=\"false\">",  sep="", file=f)
+print(iS, "<geospatialInspectionData inspectionDate=\"",dateSet,"\"", " units=\"English\" level=\"SAMPLE\" >", sep="", file=f)
+print(iS*2, "<inspectedElement inspectedElementID=\"", row[SAMPLENUMBER], "\"", " size=\"", inspectedSize, "\" ", "PID=\"", fullpid, "\" inspectedType=\"R\" comment=\"",DistressComment,  "\" noDistresses=\"false\">",  sep="", file=f)
 
 
 #if row[ALLIGATOR_S] > 0 or row[POTHOLE_S] > 0:
@@ -82,7 +78,7 @@ print (iS*3, "<inspectionData>", sep="", file=f)
 #check if severity is L,M,H (1,2,3) and write that data
 #later: need to check ad or statements for all distresses?
 
-print("Alligator/Pothole Extent and severity: AES: ", row[ALLIGATOR_E],row[ALLIGATOR_S],"PES: ", row[POTHOLE_E],row[POTHOLE_S], " \n")
+
 
 
 
@@ -109,9 +105,9 @@ distressCodes = {
 	'edgecrackC': row[EDGECRACKING_CODE],
 	'edgecrackS': row[EDGECRACKING_S],
 	'edgecrackQ': row[EDGECRACKING_Q],
-	'jointspallC': row[JOINTSPALLINGG_CODE],
-	'jointspallS': row[JOINTSPALLINGG_S],
-	'jointspallQ': row[JOINTSPALLINGG_Q],
+	'jointspallC': row[JOINTSPALLING_CODE],
+	'jointspallS': row[JOINTSPALLING_S],
+	'jointspallQ': row[JOINTSPALLING_Q],
 	'durabilityC': row[DURABILITYCRACKING_CODE],
 	'durabilityS': row[DURABILITYCRACKING_S],
 	'durabilityQ': row[DURABILITYCRACKING_Q],
@@ -121,8 +117,8 @@ distressCodes = {
 	'patchingC': row[PATCHING_CODE],
 	'patchingS': row[PATCHING_S],
 	'patchingQ': row[PATCHING_Q],
-	'bumpsagC': row[BUMPSAG_CODE]
-	'bumpsagS': row[BUMPSAG_S]
+	'bumpsagC': row[BUMPSAG_CODE],
+	'bumpsagS': row[BUMPSAG_S],
 	'bumpsagQ': row[BUMPSAG_Q]
 }
 
@@ -138,16 +134,30 @@ print (distressCheck)
 def distressPrint(code, severity, quantity):
 	try:
 		if code > 0:
+		
 			print (iS*6, "<levelDistress distressCode=\"", code, "\"", "severity=\"", severity, "\" quantity=\"", quantity, "\"", "comment=\"", DistressComment, "\" />", sep="", file=f)
+		else:
+			print ("ELSE")
+			
 	except TypeError:
 		print("TYPEERROR")
 
 
-#check if any codes are > 0 to skip empty lines
-if any([v > 0 for v in distressCheck]):
-		print (iS*5, "<PCIDistresses>", sep="", file=f)
-		distressPrint(distressCodes["sweatherC"],distressCodes["sweatherC"],distressCodes["sweatherC"])
-		print (iS*5, "</PCIDistresses>", sep="", file=f)
+
+print (iS*5, "<PCIDistresses>", sep="", file=f)
+distressPrint(distressCodes["sweatherC"],distressCodes["sweatherS"],distressCodes["sweatherQ"])
+distressPrint(distressCodes["alligatorC"],distressCodes["alligatorS"],distressCodes["alligatorQ"])
+distressPrint(distressCodes["blockcrackC"],distressCodes["blockcrackS"],distressCodes["blockcrackQ"])
+distressPrint(distressCodes["trasnverseC"],distressCodes["trasnverseS"],distressCodes["trasnverseQ"])
+distressPrint(distressCodes["depressionC"],distressCodes["depressionS"],distressCodes["depressionQ"])
+distressPrint(distressCodes["edgecrackC"],distressCodes["edgecrackS"],distressCodes["edgecrackQ"])
+distressPrint(distressCodes["jointspallC"],distressCodes["jointspallS"],distressCodes["jointspallQ"])
+distressPrint(distressCodes["durabilityC"],distressCodes["durabilityS"],distressCodes["durabilityQ"])
+distressPrint(distressCodes["durabilityC"],distressCodes["durabilityS"],distressCodes["durabilityQ"])
+distressPrint(distressCodes["faultC"],distressCodes["faultS"],distressCodes["faultQ"])
+distressPrint(distressCodes["patchingC"],distressCodes["patchingS"],distressCodes["patchingQ"])
+distressPrint(distressCodes["bumpsagC"],distressCodes["bumpsagS"],distressCodes["bumpsagQ"])
+print (iS*5, "</PCIDistresses>", sep="", file=f)
 
 
 #close initial opened xml tags
@@ -159,14 +169,14 @@ print(iS, "</geospatialInspectionData>", sep="", file=f)
 
 #possibly unneeded, Paver seems to calculate simply based on distresses.
 #unpaved roads will show PQI of "0," rating the road failed
-if row[PCI_DISTRESS] > 0:
-			print (iS, "<geospatialInspectionData level=\"SECTION\" units=\"English\" inspectionDate=\"", dateSet, "\" >", sep="", file=f)
-			print (iS*2, "<inspectedConditions PID=\"", fullpid, "\" >", sep="", file=f)
-			print (iS*3, "<conditions>", sep="", file=f)
-			print (iS*4, "<levelCondition comment=\"\" source=\"\" cndMeasureUID=\"StructPCI\" cndMeasure=\"SCI\" conditionText=\"\" condition=\"", row[PCI_DISTRESS], "\"/>", sep="", file=f)
-			print (iS*3, "</conditions>", sep="", file=f)
-			print (iS*2, "</inspectedConditions>", sep="", file=f)
-			print (iS, "</geospatialInspectionData>", sep="", file=f)
+
+print (iS, "<geospatialInspectionData level=\"SECTION\" units=\"English\" inspectionDate=\"", dateSet, "\" >", sep="", file=f)
+print (iS*2, "<inspectedConditions PID=\"", fullpid, "\" >", sep="", file=f)
+print (iS*3, "<conditions>", sep="", file=f)
+print (iS*4, "<levelCondition comment=\"\" source=\"\" cndMeasureUID=\"StructPCI\" cndMeasure=\"SCI\" conditionText=\"\" condition=\"", condition, "\"/>", sep="", file=f)
+print (iS*3, "</conditions>", sep="", file=f)
+print (iS*2, "</inspectedConditions>", sep="", file=f)
+print (iS, "</geospatialInspectionData>", sep="", file=f)
 
 #close the exec sheet
 
